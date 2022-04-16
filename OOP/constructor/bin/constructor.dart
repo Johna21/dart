11dfa17eb3dffbe6b8ee99cdaa0f37fbe478e1d7
Johna21 <58895485+Johna21@users.noreply.github.com>
2022-04-16 20:08:31 +0300

    update

--- OOP/constructor/bin/constructor.dart
+++ OOP/constructor/bin/constructor.dart
@@ -1,8 +1,41 @@
+import 'dart:developer';
+
 import 'package:constructor/constructor.dart' as constructor;
 
 /// the constructor name should be the same as the class name
 /// the constructor doesn't have explicit return type
 /// 
+/// there is 3 types of constructor
+/// default or non-arg constructor
+/// parameter constructor
+/// named constructor
+/// 
+
+
+void main(List<String> arguments) {
+   Country country = new Country();
+
+
+  var std = student(21, 'jo123', 'Jhon ');
+
+  CountryName ctry = new CountryName();
+
+  CountryName ctry2 = new CountryName.named('Ethiopia');
+
+
+}
+
+
+// defalt constrtor example
+
+class Country{
+  Country(){
+    print("default constructor example: this is Ethiopia");
+  }
+}
+
+// parametrized type constructor
+
 
 class  student {
   var studname;
@@ -14,12 +47,18 @@ class  student {
   print('the roll number is $rollnum');
   }
 
-   
 }
 
-void main(List<String> arguments) {
+// Named Constructor type
 
-  var std = student(21, 'jo123', 'Jhon ');
+/// the named constructor are used to declare the multiple constructors in single class
 
-
+class CountryName{
+  CountryName(){
+    print('default constructor from named: Ethiopia');
+  }
+  CountryName.named(String branch){
+    print('from named constructor; the country is $branch');
+  }
 }
+
