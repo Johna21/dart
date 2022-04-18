efe9d5d4ee24475abd8670c35b00d3dadc5eabec
Johna21 <58895485+Johna21@users.noreply.github.com>
2022-04-18 08:44:02 +0300

    dart library

---
+++ dar advanced/dart_library/bin/dart_library.dart
@@ -0,0 +1,8 @@
+import 'package:dart_library/dart_library.dart' ;
+
+void main(List<String> arguments) {
+  var num1 = 10;
+  var num2 = 20;
+  var sum = add(num1, num2);
+  print('dart library: ${sum}!');
+}
---
+++ dar advanced/dart_library/lib/dart_library.dart
@@ -0,0 +1,8 @@
+
+import 'dart:math';
+
+   int add(int num1, int num2){
+     print('insid calculator addition');
+     return num1+num2;
+  }
+
